36ff8c012910faf276dea36cf903c7b151bf3852
winterom <winterom@gmail.com>
2025-01-13 08:05:33 +0300

    fix: formGroup

--- angular/src/app/pages/certificates/certificates.component.ts
+++ angular/src/app/pages/certificates/certificates.component.ts
@@ -1,17 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {CallUsComponent} from '../../components/call-us/call-us.component';
 import {OurWorksComponent} from '../../components/our-works/our-works.component';
+import {CertificatesService} from '../../services/certificates.service';
+import {NgClass, NgForOf, NgOptimizedImage} from '@angular/common';
+import {CallRequestComponent} from '../../components/call-request/call-request.component';
+import {ModalService} from '../../components/modal/modal.service';
 
 @Component({
   selector: 'app-certificates',
   imports: [
     CallUsComponent,
-    OurWorksComponent
+    OurWorksComponent,
+    NgOptimizedImage,
+    NgForOf,
+    NgClass
   ],
   templateUrl: './certificates.component.html',
   standalone: true,
-  styleUrl: './certificates.component.scss'
+  styleUrls: ['./certificates.component.scss','../../commons/styles/buttons.scss','../../commons/styles/catalog.scss','../../commons/styles/title.scss']
 })
-export class CertificatesComponent {
+export class CertificatesComponent implements OnInit{
+  urls:string[]=[]
+  currentUrl:number=0
+  constructor(private certificatesService:CertificatesService,private modalService:ModalService) {}
+
+  ngOnInit(): void {
+    this.certificatesService.getCertificatesUrls().subscribe(urls=>{
+      this.urls=urls
+    })
+  }
+
+  requestCallShow() {
+    this.modalService.open(CallRequestComponent)
+  }
 
 }
--- angular/src/app/services/certificates.service.ts
+++ angular/src/app/services/certificates.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import {Observable, of} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CertificatesService {
 
-  constructor() { }
+  getCertificatesUrls():Observable<string[]>{
+    return of(certificatesUrls)
+  }
 }
+
+const certificatesUrls:string[]=['img/certificates/certificate1.jpg']
