c4f2ac35d89dd7db8f60d41daeba89eb9597e69d
winterom <winterom@gmail.com>
2025-01-24 12:49:03 +0300

    feature: our works

--- angular/src/app/services/HttpApi.ts
+++ angular/src/app/services/HttpApi.ts
@@ -5,7 +5,7 @@ import {Injectable} from '@angular/core';
   providedIn: 'root'
 })
 export class HttpApi extends RootApi{
-  protocol: string='http';
+  protocol: string='https';
   services: string = 'services'
 
   getAllServiceApi():string{
--- angular/src/app/services/RootApi.ts
+++ angular/src/app/services/RootApi.ts
@@ -4,9 +4,9 @@ export abstract class RootApi{
   private app_prefix:string='application';
   private api_version:string='api/v1';
   private zone:string='site'
-  private port:string='80';
+
 
   getRoot():string{
-    return this.protocol+'://'+this.host + ':' +this.port + '/' + this.app_prefix + '/' + this.api_version+'/' + this.zone;
+    return this.protocol+'://'+this.host  + '/' + this.app_prefix + '/' + this.api_version+'/' + this.zone;
   }
 }
